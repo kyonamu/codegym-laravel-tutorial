5bff6d0496a36b97610fe83c3e8d27c279a9f045
kyonamu <xiaochuanmunan@gmail.com>
2021-12-01 19:54:44 +0900

    その他の修正

---
+++ application/app/Providers/ValidatorServiceProvider.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Validator;
+
+class ValidatorServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        Validator::extend(
+            'max',
+            function ($attribute, $value, $parameters, $validator) {
+                return (number_format($value));
+            }
+        );
+    }
+}
